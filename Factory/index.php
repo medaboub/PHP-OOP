dc5230b846544f80371cea41de238f9cabed118e
BureauTT <m.gaddour@yahoo.fr>
2022-04-18 22:48:00 +0100

    add static Factory pattern

---
+++ Factory/index.php
@@ -0,0 +1,20 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+spl_autoload_register(function($class){
+    include $class .".php";
+});
+
+/** when JSON Likes Notification arrive {'type':'Likes'} **/
+$notification= NotificationFactory::BuildNotification("Likes");
+$notification->notify();
+
+/** when new custom notification arrive **/
+$notification= NotificationFactory::BuildNotification("Custom");
+$notification->notify();
